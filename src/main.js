8cb75abd81fd461c0f07cff1ff1f75baa50775f5
Mateo <cyp3rbot@outlook.com>
2022-09-30 22:43:33 -0400

    add more code

--- src/main.js
+++ src/main.js
@@ -11,18 +11,34 @@ fetch('./words.json')
             })
     })
 
-const alphabet = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+const alphabet = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","ENTER"];
 document.addEventListener('keydown', function(event) {
-    if (alphabet.includes(event.key.toUpperCase())) { // Handle key press
+    const boxes = document.getElementsByClassName("box");
 
-        const boxes = document.getElementsByClassName("box");
+    if (alphabet.includes(event.key.toUpperCase())) { // Handle letter press
+
+        if (event.key.toUpperCase() === "ENTER") {
+
+            for (let n=0; n < boxes.length; n++) {
+                let iteration = boxes.length - n - 1;
+                if (boxes[iteration].textContent !== "") {
+                    if ([5,10,15,20,25,30].includes(iteration+1)) {
+                        console.log("Submitted word");
+                    }
+                    return;
+                }
+            }
+            return;
+        }
 
         for (let n=0; n < boxes.length; n++) {
-            let iteration = n;
 
-            if (boxes[iteration].textContent === "") {
-                boxes[iteration].textContent = event.key.toUpperCase();
-                break;
+            if (boxes[n].textContent === "") {
+                if (![5,10,15,20,25,30].includes(n)) {
+                    boxes[n].textContent = event.key.toUpperCase();
+                    return;
+                }
+                return;
             }
         }
 
