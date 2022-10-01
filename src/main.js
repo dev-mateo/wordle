15b66a053e1e9b77b179f3711e8378d20d7de39d
Mateo <cyp3rbot@outlook.com>
2022-09-30 22:15:42 -0400

    i slaved for 2 hours

--- src/main.js
+++ src/main.js
@@ -11,7 +11,34 @@ fetch('./words.json')
             })
     })
 
+const alphabet = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+document.addEventListener('keydown', function(event) {
+    if (alphabet.includes(event.key.toUpperCase())) { // Handle key press
 
+        const boxes = document.getElementsByClassName("box");
+
+        for (let n=0; n < boxes.length; n++) {
+            let iteration = n;
+
+            if (boxes[iteration].textContent === "") {
+                boxes[iteration].textContent = event.key.toUpperCase();
+                break;
+            }
+        }
+
+    } else if (event.key === "Delete" || event.key === "Backspace") { // Handle delete key
+        const boxes = document.getElementsByClassName("box");
+
+        for (let n=0; n < boxes.length; n++) {
+            let iteration = boxes.length - n - 1;
+            if (boxes[iteration].textContent !== "") {
+                boxes[iteration].textContent = "";
+                break;
+            }
+        }
+    }
+
+});
 
 
 
